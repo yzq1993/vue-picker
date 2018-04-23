68858182d7890470f4b8edcef37333b6893c8c76
yzq1993 <541866001@qq.com>
2018-04-23 12:00:01 +0800

    Update index.js

--- src/vue-picker/multipicker/index.js
+++ src/vue-picker/multipicker/index.js
@@ -1,20 +1,23 @@
 import Vue from 'vue';
 import picker from '../picker'
 let multipicker=(options={})=>{
-	let option=getOption(options.option,getIndex(options.option,options.value)),
-		hasValue=options.value&&options.value.length!=0,len
+	options=Object.assign({value:[],title:'请选择'},options)
+	let option=getOption(options.option,options.value);
 	return new Promise((resolve, reject)=>{
 		picker({
-			title:options.title||'请选择',
+			title:options.title,
 			value:options.value,
 			option:option,
 			shadeClose:options.shadeClose,
 			change:(value,index,_this)=>{
-				let newOption=changeOption(options.option,value)
-				option.splice(++index,value.length-index,...newOption)
-				options.value.splice(index,value.length-index)
-				newOption.forEach((m)=>{
-					_this.scrollTop(index++,m[0])
+				let newOption=getOption(options.option,value)
+				option.splice(0,option.length,...newOption)
+				value.splice(index+1,value.length-index-1)
+				option.forEach((m,key)=>{
+					if(key>index){
+						value.push(m[0])
+						_this.scrollTop(key,m[0])
+					}
 				})
 				if(typeof options.change =='function'){
 					options.change(value,index,_this)
@@ -28,12 +31,11 @@ let multipicker=(options={})=>{
 	})
 }
 
-export default  multipicker
-
-
+export default multipicker
 
 function getOption(options,value,arr=[]){
-	let len=arr.length,index=value[len]||0
+	let len=arr.length,index=options.findIndex(v=>v.name==value[len])
+	index=index>-1?index:0
 	arr[len]=[]
 	options.forEach((m)=>{
 		arr[len].push(m.name)
@@ -42,42 +44,5 @@ function getOption(options,value,arr=[]){
 		return getOption(options[index].children,value,arr)
 	}else{
 	 	return arr
-	}
-}
-function changeOption(options,value,arr=[],index=getIndex(options,value)){
-	if(index.length){
-		if(index.length==1){
-			arr=addOption(options,index[0])
-		}
-		let m=index.splice(0,1)[0]
-		return changeOption(options[m].children,value,arr,index)
-	}else{
-		return arr
-	}
-}
-function addOption(options,index,arr=[]){
-	let len=arr.length
-	if(options[index].children){
-		arr[len]=[]
-		options[index].children.forEach((m)=>{
-			arr[len].push(m.name)
-		})
-		return addOption(options[index].children,0,arr)
-	}else{
-		return arr
-	}
-}
-function getIndex(options,value,arr=[]){
-	let len=arr.length
-	options.forEach((m,index)=>{
-		if(m.name==value[len]){
-			arr[len]=index
-		}
-	})
-	
-	if(arr.length>len&&options[arr[len]].children){
-		return getIndex(options[arr[len]].children,value,arr)
-	}else{
-	 	return arr
-	}
+	} 
 }
